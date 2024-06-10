cf03a13cce2a258ff4b64d7c516c0a44a12665d0
Maxime <57860498+MaximeMRF@users.noreply.github.com>
2024-06-10 15:06:47 +0800

    fix(password): allow high length

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const loginValidator = vine.compile(
   vine.object({
     email: vine.string().email(),
-    password: vine.string().minLength(8).maxLength(32),
+    password: vine.string().minLength(12).maxLength(512),
   })
 )
 
@@ -17,6 +17,6 @@ export const registerValidator = vine.compile(
         const user = await query.from('users').where('email', field).first()
         return !user
       }),
-    password: vine.string().minLength(8).maxLength(32),
+    password: vine.string().minLength(12).maxLength(512),
   })
 )
