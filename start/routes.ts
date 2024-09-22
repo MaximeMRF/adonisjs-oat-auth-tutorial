15ca54c211a7b93f925f449ecc524f08063d3cf0
MaximeMRF <maxime.roth-fessler@epitech.eu>
2024-09-22 23:32:08 +0800

    fix: linter

--- start/routes.ts
+++ start/routes.ts
@@ -3,18 +3,21 @@ import { middleware } from './kernel.js'
 
 const AuthController = () => import('#controllers/auth_controller')
 
-router.group(() => {
-  router.post('register', [AuthController, 'register'])
-  router.post('login', [AuthController, 'login'])
-  router.post('logout', [AuthController, 'logout']).use(middleware.auth())
-}).prefix('user')
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
 
-router.get('me', async ({ auth, response }) => {
-  try {
-    const user = auth.getUserOrFail()
-    return response.ok(user)
-  } catch (error) {
-    return response.unauthorized({ error: 'User not found' })
-  }
-})
-.use(middleware.auth())
+router
+  .get('me', async ({ auth, response }) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({ error: 'User not found' })
+    }
+  })
+  .use(middleware.auth())
